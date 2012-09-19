bed868812ffaf57cc382bd058f567301daeca6cd
Javier Acero <j4cegu@gmail.com>
2012-09-19 23:23:16 +0200

    page range specs and refactoring

--- lib/hexabat/page_range.rb
+++ lib/hexabat/page_range.rb
@@ -1,4 +1,62 @@
+require 'delegate'
+
 module Hexabat
-  class PageRange
+  module PageRange
+    def self.from(headers)
+      return SinglePageRange.new(1) unless headers.has_key? 'LINK'
+      return MultiplePageRange.new(1, last_page_from(headers['LINK']))
+    end
+
+    def self.last_page_from(link_header)
+      link_header.scan(/\?page=(?'page_number'\d+)/)[1][0].to_i
+    end
+  end
+
+  class MultiplePageRange < DelegateClass(Range)
+    def initialize(first, last)
+      super(first..last)
+    end
+
+    def page_count
+      last - first + 1
+    end
+
+    def middle
+      if page_count > 2
+        MultiplePageRange.new(first.succ, last.pred)
+      else
+        SinglePageRange.new(first.succ)
+      end
+    end
+
+    def middle_page_count
+      middle.page_count
+    end
+
+    def multiple_pages?
+      true
+    end
+  end
+
+  class SinglePageRange < DelegateClass(Range)
+    def initialize(page)
+      super page..page
+    end
+
+    def page_count
+      1
+    end
+
+    def middle
+      first...last
+    end
+
+    def middle_page_count
+      0
+    end
+
+    def multiple_pages?
+      false
+    end
   end
 end
---
+++ spec/hexabat/page_range_spec.rb
@@ -0,0 +1,77 @@
+require 'hexabat/page_range'
+
+describe Hexabat::PageRange do
+  context 'building page ranges from headers' do
+    it 'creates a SinglePageRange if there is only one page' do
+      headers = { 'NO LINK' => '' }
+      Hexabat::SinglePageRange.should_receive(:new).with(1)
+      described_class.from(headers)
+    end
+
+    it 'creates a MultiplePageRange if there is more than one page' do
+      headers = { 'LINK' => '<https://xxx.com?page=2&per_page=100>; rel="next", <https://xxx.com?page=5&per_page=100>; rel="last"' }
+      Hexabat::MultiplePageRange.should_receive(:new).with(1, 5)
+      described_class.from(headers)
+    end
+  end
+end
+
+describe Hexabat::MultiplePageRange do
+
+  it 'knows the first page' do
+   described_class.new(3, 5).first.should be 3
+  end
+
+  it 'knows the page count' do
+    described_class.new(1, 2).page_count.should be 2
+    described_class.new(5, 9).page_count.should be 5
+  end
+
+  it 'knows the last page' do
+   described_class.new(1, 5).last.should be 5
+  end
+
+  it 'knows the middle pages' do
+   described_class.new(1, 2).middle.should eq 2..2
+   described_class.new(1, 9).middle.should eq 2..8
+  end
+
+  it 'knows the middle pages count' do
+    described_class.new(1, 2).middle_page_count.should eq 1
+    described_class.new(1, 9).middle_page_count.should eq 7
+  end
+
+  it 'knows if it has more than one page' do
+    described_class.new(1, 2).multiple_pages?.should be_true
+    described_class.new(1, 5).multiple_pages?.should be_true
+  end
+end
+
+describe Hexabat::SinglePageRange do
+  subject    { described_class.new page }
+  let(:page) { 2 }
+
+  it 'knows the first page' do
+   subject.first.should be 2
+  end
+
+  it 'knows the page count' do
+    subject.page_count.should be 1
+  end
+
+  it 'knows the last page' do
+    subject.last.should be 2
+  end
+
+  it 'knows the middle pages' do
+    subject.middle.should eq 2...2
+  end
+
+  it 'knows the middle pages count' do
+    subject.middle_page_count.should eq 0
+  end
+
+  it 'knows if it has more than one page' do
+    subject.multiple_pages?.should be_false
+  end
+end
