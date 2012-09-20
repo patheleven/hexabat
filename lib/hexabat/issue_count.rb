f85780edefffdef7f0395cae2558fe899484b874
Javier Acero <j4cegu@gmail.com>
2012-09-20 11:05:29 +0200

    specs for the issue count

--- lib/hexabat/issue_count.rb
+++ lib/hexabat/issue_count.rb
@@ -1,6 +1,49 @@
 module Hexabat
   class IssueCount
+    ISSUES_PER_PAGE = 100
+
+    attr_reader :page_ranges, :issue_counts
+
     def initialize(&issue_count_known)
+      @count_known  = issue_count_known
+      @page_ranges  = { open: nil, closed: nil }
+      @issue_counts = { open: {first: nil, last: nil}, closed: {first: nil, last: nil}}
     end
+
+    def counted(page, state, page_range, issue_count)
+      check_done_counting do
+        @page_ranges[state] = page_range
+        @issue_counts[state][page] = issue_count
+        @issue_counts[state][:last] = 0 unless page_range.multiple_pages?
+      end
+    end
+
+    private
+
+    def check_done_counting
+      yield
+      @count_known.call(count) if all_done?
+    end
+
+    def all_done?
+      done_counting_issues? :open and done_counting_issues? :closed
+    end
+
+    def count
+      issue_count(:open) + issue_count(:closed)
+    end
+
+    def done_counting_issues?(state)
+      not @page_ranges[state].nil? and
+      not @issue_counts[state][:first].nil? and
+      not @issue_counts[state][:last].nil?
+    end
+
+    def issue_count(state)
+      @issue_counts[state][:first] +
+      @issue_counts[state][:last] +
+      @page_ranges[state].middle_page_count * ISSUES_PER_PAGE
+    end
+
   end
 end
---
+++ spec/hexabat/issue_count_spec.rb
@@ -0,0 +1,42 @@
+require 'hexabat/issue_count'
+
+describe Hexabat::IssueCount do
+  subject                 { described_class.new &issue_count_known }
+  let(:issue_count_known) { lambda{|count| @count = count} }
+
+  it 'keeps track of the notified page ranges' do
+    subject.page_ranges.should eq Hash[open: nil, closed: nil]
+  end
+
+  it 'keeps track of the notified issue counts' do
+    initial_count = {
+      open:   { first: nil, last: nil },
+      closed: { first: nil, last: nil }
+    }
+    subject.issue_counts.should eq initial_count
+  end
+
+  it 'can be notified with issue counts and page ranges' do
+    page_range = stub(:page_range, multiple_pages?: true)
+    subject.counted :first, :open, page_range, 10
+    subject.page_ranges.should  eq Hash[open: page_range, closed: nil]
+    expected_count = {open: {first: 10, last: nil}, closed: {first: nil, last: nil}}
+    subject.issue_counts.should eq expected_count
+  end
+
+  it 'updates issue counts if there is only one page' do
+    page_range = stub(:page_range, multiple_pages?: false, middle_page_count: 0)
+    subject.counted :first, :open, page_range, 2
+    expected_count = {open: {first: 2, last: 0}, closed: {first: nil, last: nil}}
+    subject.issue_counts.should eq expected_count
+  end
+
+  it 'calls the provided callback when done counting' do
+    open_page_range   = stub(:page_range, multiple_pages?: false, middle_page_count: 0)
+    closed_page_range = stub(:page_range, multiple_pages?: true, middle_page_count: 2)
+    subject.counted :first, :open, open_page_range, 15
+    subject.counted :first, :closed, closed_page_range, 100
+    subject.counted :last, :closed, closed_page_range,  10
+    @count.should be 325
+  end
+end
